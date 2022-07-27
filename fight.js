94b6286df7cf0fa967172adaac5b37f30ab92738
grossesbewirken <grossesbewirken@gmail.com>
2022-07-27 20:22:17 +0200

    fight.js hinzugefügt

---
+++ fight.js
@@ -0,0 +1,26 @@
+function fight(enemy) {
+    enemy.life -= this.attack;
+    console.log(`${this.name} fight ${enemy.name}(${enemy.life})`);
+  }
+  
+  const doomGuy = {
+    name: 'Doomguy',
+    life: 10,
+    attack: 2,
+    fight
+  };
+  
+  const cyberDemon = {
+    name: 'Cyberdemon',
+    life: 15,
+    attack: 1,
+    fight
+  };
+  
+  doomGuy.fight(cyberDemon);
+  doomGuy.fight(cyberDemon);
+  cyberDemon.fight(doomGuy);
+  doomGuy.fight(cyberDemon);
+  cyberDemon.fight(doomGuy);
+  cyberDemon.fight(doomGuy);
+  doomGuy.fight(cyberDemon);
