5719138c731a1cbb9290e22845f2b604930aaf84
grossesbewirken <grossesbewirken@gmail.com>
2022-07-27 19:14:29 +0200

    initial commit

---
+++ fighters.js
@@ -0,0 +1,120 @@
+const dinos = [
+    {
+        name: "Ankylosaurus",
+        nickname: "Der lebende Panzer",
+
+        weight: 7500,
+        height: 170,
+        length: 800,
+
+        live: 100,
+        minDamage: 100,
+        maxDamage: 100,
+
+        critlName: "Rolling Stone",
+        crit: 100,
+    },
+    {
+        name: "Dilophosaurus",
+        nickname: "Die Giftspritze",
+
+        weight: 450,
+        height: 300,
+        length: 700,
+
+        critName: "Black Eyed Piece",
+        crit: 100,
+    },
+    {
+        name: "Pteranodon",
+        nickname: "Der Tod aus der Luft",
+
+        weight: 35,
+        height: 300,
+        length: 350,
+        wings: 1100,
+
+        critName: "Gravity Sucks",
+        crit: 100,
+    },
+    {
+        name: "Tyrannosaurus Rex",
+        nickname: "Der König",
+
+        weight: 6000,
+        height: 600,
+        length: 1000,
+
+        critName: "Loud 'n' Proud",
+
+    },
+    {
+        name: "Veloceraptor",
+        nickname: "Die Sichelkralle",
+
+        weight: 15,
+        height: 300,
+        length: 250,
+
+        critName: "Band of Brothers",
+        crit: 100,
+    }
+];
+
+
+
+
+
+const monks = [
+    {
+        name: "Rock Solid Rocky",
+        nickname: "Yú Ken Tmuh Vmí",
+
+        weight: 180,
+        height: 200,
+
+        critName: "Band of Brothers",
+        crit: 100,
+    },
+    {
+        name: "Chopstick Charlie",
+        nickname: "",
+
+        weight: 80,
+        height: 170,
+
+        critName: "Bulls Eye",
+        crit: 100,
+    },
+    {
+        name: "",
+        nickname: "",
+
+        weight: 95,
+        height: 190,
+
+        critName: "",
+        crit: 100,
+    },
+    {
+        name: "Thunder Punch Theo",
+        nickname: "",
+
+        weight: 95,
+        height: 180,
+
+        critName: "",
+        crit: 100,
+    },
+    {
+        name: "Longsword Lewis",
+        nickname: "",
+        specialName: "",
+
+        weight: 110,
+        height: 200,
+
+        critName: "",
+        crit: 100,
+    }
+];
