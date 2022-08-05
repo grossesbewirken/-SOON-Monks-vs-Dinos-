19650c9b454783b3341057b375449556f51f0d56
grossesbewirken <grossesbewirken@gmail.com>
2022-08-05 10:32:16 +0200

    erste Versuche fight

--- fight.js
+++ fight.js
@@ -1,26 +1,45 @@
-import dinoVeloceraptor from "./fighters.js";
+// import dinoVeloceraptor from "./fighters.js";
 
+
+// function fight(enemy) {
+//   enemy.life -= this.attack;
+//   console.log(`${this.name} fight ${enemy.name}`);
+//   console.log(`${this.name}-life: ${this.life}`);
+//   console.log(`${enemy.name}-life: ${enemy.life}`);
+// };
 
 
 
 function fight(enemy) {
-  enemy.life -= this.attack;
-  console.log(`${this.name} fight ${enemy.name}(${enemy.life})`);
-};
+  for (let i = 0; i < 10; i++) {
+    console.log(`\n\n---------------- Runde ${i+1} ----------------\n`);
+    if (this.life < 1 ){
+      console.log(`${this.name} was killed by ${enemy.name}`);
+      break;
+    } else {
+        enemy.life -= this.attack;
+        console.log(`${this.name} fight ${enemy.name}`);
+        console.log(`${this.name}-life: ${this.life}`);
+        console.log(`${enemy.name}-life: ${enemy.life}`);
+    }
+  }
+}
 
 
-const doomGuy = {
-  name: 'Doomguy',
+const fighter1 = {
+  name: 'fighter1',
   life: 10,
   attack: 2,
   fight
 };
 
-const cyberDemon = {
-  name: 'Cyberdemon',
+
+const fighter2 = {
+  name: 'fighter2',
   life: 15,
   attack: 1,
   fight
 };
 
-doomGuy.fight(cyberDemon);
+
+fighter1.fight(fighter2);
